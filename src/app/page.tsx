0bdf58d42c84592c26f18a8ac62ee895909749c7
lautarolamas <lautilamas7@gmail.com>
2025-01-06 16:21:46 -0300

    feat: add metrics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ import Hero from "@/components/ui/sections/Hero";
 import Services from "@/components/ui/sections/Services";
 import Pricing from "@/components/ui/sections/Pricing";
 // import Testimonials from "@/components/ui/sections/Testimonials";
+
 import FAQ from "@/components/ui/sections/FAQ";
 import Footer from "@/components/ui/sections/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Page() {
   return (
@@ -16,6 +19,8 @@ export default function Page() {
       <Services />
       <Pricing />
       {/* <Testimonials /> */}
+      <SpeedInsights />
+      <Analytics />
       <FAQ />
       <Footer />
     </div>
